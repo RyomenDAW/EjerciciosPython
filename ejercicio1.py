6e2c8adc5cc705a68d487eabe7677795ad58bd2b
Israel Hernandez <israel.hernandez.chacon.al@iespoligonosur.org>
2024-09-24 12:08:12 +0200

    BoletinZip

---
+++ ejercicio1.py
@@ -0,0 +1,24 @@
+print ("Hola clase DAW")
+numero = 25
+if numero >= 18:
+    print("Es mayor de edad")
+
+
+
+for i in range (6,22):
+    print(i)
+print("Fin")
+
+
+for i in range(0,10):
+    for j in range(0,10):
+        print("posicion "+str(i)+","+str(j))
+print("Terminamos el for doble")
+
+
+lista = [1,7,8,9]
+for x in lista:
+    print("Elemento de la lista "+str(x))
+print("Hemos terminado")
+
+#
